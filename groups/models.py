7c35a8528db9e564d63bfb57a8349104e2062b73
Hobinx <binservmail@gmail.com>
2017-12-10 19:55:29 +0800

    install misaka, and Group and GroupMember model stab

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+from django import template
+import misaka
 
-# Create your models here.
+
+User = get_user_model()
+
+register = template.Library()
+
+
+class Group(models.Model):
+    pass
+
+
+class GroupMember(models.Model):
+    pass
