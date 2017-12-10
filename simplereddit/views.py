9d875ccea3824dcd670841b3368756f72a74ebbc
Hobinx <binservmail@gmail.com>
2017-12-10 19:41:21 +0800

    migrate User and make test view for login and logout

--- simplereddit/views.py
+++ simplereddit/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
 
+class TestPage(TemplateView):
+    template_name = 'simplereddit/test.html'
+
+
+class ThanksPage(TemplateView):
+    template_name = 'simplereddit/thanks.html'
+
+
 class HomePage(TemplateView):
     template_name = 'simplereddit/index.html'
